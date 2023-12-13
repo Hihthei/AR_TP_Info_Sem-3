b76b8fd48c0aa63197a1cfbbb42b5332a09d1f41
Hihthei <cbouillon@et.esiea.fr>
2023-12-13 23:28:27 +0100

    dataset setup_1 - NOT FINISHED

--- TP_Info_sem-3/Dataset_c.c
+++ TP_Info_sem-3/Dataset_c.c
@@ -1,1 +1,52 @@
 #include "Dataset_h.h"
+
+Dataset* Dataset_readFromFile(char* filename) {
+	if (filename == NULL)
+		return NULL;
+		
+	FILE* pfile = NULL;
+	pfile = fopen(filename, "r");
+
+	if (pfile == NULL)
+		return NULL;
+
+	Dataset* dataset = calloc(1, sizeof(Dataset));
+
+	int tmp = fscanf(pfile, "%d %d %d\n", &(dataset->instanceCount),
+								&(dataset->featureCount),
+								&(dataset->classCount));
+	return dataset;
+}
+
+void Dataset_destroy(Dataset* data) {
+	// TODO
+	return;
+}
+
+Subproblem* Dataset_getSubproblem(Dataset* data) {
+	// TODO
+	return NULL;
+}
+
+Subproblem* Subproblem_create(	int maximumCapacity,
+								int featureCount,
+								int classCount) {
+	// TODO
+	return NULL;
+}
+
+void Subproblem_destroy(Subproblem* subproblem) {
+	// TODO
+	return;
+}
+
+void Subproblem_insert(	Subproblem* subproblem,
+						Instance* instance) {
+	// TODO
+	return;
+}
+
+void Subproblem_print(Subproblem* subproblem) {
+	// TODO
+	return;
+}
--- TP_Info_sem-3/main.c
+++ TP_Info_sem-3/main.c
@@ -1,4 +1,5 @@
 #include "ABR_h.h"
+#include "Dataset_h.h"
 
 int main(int argc, char** argv)
 {
