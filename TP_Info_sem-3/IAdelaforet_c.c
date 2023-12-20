b27b19f4404f7236e2af54fe2378f1eafb914965
AlyoGB <135697151+AlyoGB@users.noreply.github.com>
2023-12-20 08:46:49 +0100

    Narmol l'ia marche

--- TP_Info_sem-3/IAdelaforet_c.c
+++ TP_Info_sem-3/IAdelaforet_c.c
@@ -27,3 +27,29 @@ int DecisionTree_predict(DecisionTreeNode* tree, Instance* instance)
 		DecisionTree_predict(tree->right, instance);
 	}
 }
+
+float DecisionTree_evaluate(DecisionTreeNode* tree, Dataset* dataset)
+{
+	if (!tree)
+	{
+		printf("No tree\n");
+		return -1;
+	}
+
+	if (!dataset)
+	{
+		printf("No dataset\n");
+		return -1;
+	}
+
+	float nb_success = 0;
+	for (int i = 0; i < dataset->instanceCount; i++)
+	{
+		if (DecisionTree_predict(tree, &dataset->instances[i]) == dataset->instances[i].classID)
+		{
+			nb_success++;
+		}
+	}
+	
+	return (nb_success / (float)dataset->instanceCount);
+}
